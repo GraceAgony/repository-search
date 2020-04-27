5187cb6ebeadb71227dd8ad5ef36fb0fd67d0118
lgrabovska <theogonia8@gmail.com>
2020-04-27 17:15:08 +0300

    Updates

---
+++ precache-manifest.667fbaffc20ee4c4c6c227f1d31300fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89930a49415e5ef1a4a0d918d0fee6e2",
+    "url": "/repository-search/index.html"
+  },
+  {
+    "revision": "4246d54f3f66b17c9e62",
+    "url": "/repository-search/static/css/2.e50de653.chunk.css"
+  },
+  {
+    "revision": "d011f57311ce8672b37e",
+    "url": "/repository-search/static/css/main.96d99cb1.chunk.css"
+  },
+  {
+    "revision": "4246d54f3f66b17c9e62",
+    "url": "/repository-search/static/js/2.d133a022.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/repository-search/static/js/2.d133a022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d011f57311ce8672b37e",
+    "url": "/repository-search/static/js/main.35e69286.chunk.js"
+  },
+  {
+    "revision": "85d7a7ab5d303e7e4b75",
+    "url": "/repository-search/static/js/runtime-main.2b9dbabd.js"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/repository-search/static/media/Octocat.ef468e27.png"
+  }
+]);
